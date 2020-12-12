bbb86c7653703e6f8f7d771418edde01e613469f
Alexander.Vybornyi <iamalexvybornyi@gmail.com>
2020-12-12 22:29:57 +0400

    Add some basic components and try them

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("name");
+  const [age, setAge] = useState(40);
+
+  const clickHandler = () => {
+    setAge(45);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Update!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter name: </Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="e.g. John Doe"
+        onChangeText={(value) => setName(value)}
+      />
+
+      <Text>Enter age: </Text>
+      <TextInput
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder="e.g. 30"
+        onChangeText={(value) => setAge(value)}
+      />
+
+      <Text>The name is {name} and the age is {age}</Text>
+
+      <View style={styles.buttonContainer}>
+        <Button title="Update age" onPress={clickHandler} />
+      </View>
     </View>
   );
 }
@@ -14,8 +40,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "blue",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
